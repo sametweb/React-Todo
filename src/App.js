159d0df16565451cf1145c0b99ce8b495d8ea792
sametweb <sametmutevellioglu@gmail.com>
2020-01-15 00:37:21 -0800

    Stateful logic, controlled components, conditional style rendering, Semantic UI implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,100 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  state = {
+    error: "",
+    textInput: "",
+    toDoList: []
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      ...this.state,
+      textInput: e.target.value
+    });
+  };
+
+  addNewItem = e => {
+    e.preventDefault();
+
+    if (!this.state.textInput) {
+      this.setState({ ...this.state, error: "You must enter a task name!" });
+    } else {
+      this.setState({
+        ...this.state,
+        textInput: "",
+        error: "",
+        toDoList: [
+          ...this.state.toDoList,
+          { task: this.state.textInput, id: Date.now(), completed: false }
+        ]
+      });
+    }
+  };
+
+  markCompleted = id => {
+    this.setState({
+      ...this.state,
+      toDoList: this.state.toDoList.map(item =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    });
+  };
+
+  clearCompletedItems = () => {
+    this.setState({
+      ...this.state,
+      toDoList: this.state.toDoList.filter(item => item.completed === false)
+    });
+  };
+
   render() {
+    console.log(this.state);
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="ui two column centered grid">
+        <div className="row">
+          <div className="column">
+            <form onSubmit={this.addNewItem} onReset={this.clearCompletedItems}>
+              <div className="ui fluid action input">
+                <input
+                  onChange={this.handleInputChange}
+                  value={this.state.textInput}
+                  type="text"
+                  placeholder="new item..."
+                />
+                <button type="submit" className="ui teal button">
+                  Add Item
+                </button>
+                <button type="reset" className="ui button">
+                  Clear Completed Tasks
+                </button>
+              </div>
+              {this.state.error && (
+                <div class="ui red message">{this.state.error}</div>
+              )}
+            </form>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            {this.state.toDoList.map(item => (
+              <div
+                className={`ui fluid ${
+                  item.completed ? `green inverted` : ""
+                } vertical menu`}
+              >
+                <a
+                  key={item.id}
+                  className="item"
+                  onClick={() => this.markCompleted(item.id)}
+                >
+                  {item.task}
+                  {item.completed ? <i className="check icon white"></i> : null}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
